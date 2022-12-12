09ed883966775ca555d1c655c95bd143f4438800
Haresh G <haresh.ghatala@gmail.com>
2022-12-12 15:19:31 +0200

    Base project setup and folder structuring

---
+++ Image Search Flickr/View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Image Search Flickr
+//
+//  Created by Haresh Ghatala on 2022/12/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Image Search FlickrUITests/Image_Search_FlickrUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Image_Search_FlickrUITestsLaunchTests.swift
+//  Image Search FlickrUITests
+//
+//  Created by Haresh Ghatala on 2022/12/12.
+//
+
+import XCTest
+
+final class Image_Search_FlickrUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
