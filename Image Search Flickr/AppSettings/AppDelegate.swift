64c1accef6a380a837a0805a3b75570d95134292
Haresh Ghatala <haresh.ghatala@gmail.com>
2022-12-12 16:03:11 +0200

    Added ProgressHud in the project

--- Image Search Flickr/AppSettings/AppDelegate.swift
+++ Image Search Flickr/AppSettings/AppDelegate.swift
@@ -13,7 +13,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        ProgressHUD.animationType = .circleStrokeSpin
+        ProgressHUD.colorHUD = .white
+        ProgressHUD.colorStatus = UIColor(named: "Flickr-Blue")!
+        ProgressHUD.colorAnimation = UIColor(named: "Flickr-Pink")!
         return true
     }
 
